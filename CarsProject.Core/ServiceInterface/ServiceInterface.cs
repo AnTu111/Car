32240ef0d5daf9c2941ff42ab3f3444fb66fd3b0
admin <andrei.tupitsin@gmail.com>
2025-01-19 00:46:13 +0200

    Base for project

---
+++ CarsProject.Core/ServiceInterface/ServiceInterface.cs
@@ -0,0 +1,21 @@
+﻿using CarsProject.Core.Domain;
+using CarsProject.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarsProject.Core.Domain;
+using CarsProject.Core.Dto;
+using System;
+
+namespace CarsProject.Core.ServiceInterface
+{
+    public interface ICarsServices
+    {
+        Task<Car> Create(CarDto dto);
+        Task<Car> GetAsync(Guid id);
+        Task<Car> Delete(Guid id);
+        Task<Car> Update(CarDto dto);
+    }
+}
