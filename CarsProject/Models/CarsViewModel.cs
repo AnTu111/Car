c15319623b028ee5f0e1be04021b54cebec40883
admin <andrei.tupitsin@gmail.com>
2025-01-20 01:20:27 +0200

    Add CarsViewModel to project + Errors

---
+++ CarsProject/Models/CarsViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CarsProject.Models
+{
+    public class CarsViewModel
+    {
+        public Guid? Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string MotorPower { get; set; }
+        public string Fuel { get; set; }
+        public string Price { get; set; }
+    }
+}
